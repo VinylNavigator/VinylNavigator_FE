3d28bfd14d8f5d395a9c60809c93c9cf508f2dcb
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-28 19:08:14 -0600

    added boilerplate for postRecord form and added css file/folder for landing card styles

---
+++ client/src/forms/postRecord.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class postRecord extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>Post Record</div> 
+        )
+    }
+}
