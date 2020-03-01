6ff5719be5b93c34c5f215f82d9f1f27b86234fb
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-29 23:44:04 -0600

    added carousel to landing page

--- client/src/landing/landing.js
+++ client/src/landing/landing.js
@@ -8,7 +8,13 @@ export default class Landing extends React.Component{
     render(){
         return(
             <div>
-                
+              <div class="carousel">
+                <a class="carousel-item" href="#one!"><img src='https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'/></a>
+                <a class="carousel-item" href="#two!"><img src='https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'/></a>
+                <a class="carousel-item" href="#three!"><img src='https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'/></a>
+                <a class="carousel-item" href="#four!"><img src='https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'/></a>
+                <a class="carousel-item" href="#five!"><img src='https://images.unsplash.com/photo-1454789548928-9efd52dc4031?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'/></a>
+              </div>
             </div> 
         )
     }
