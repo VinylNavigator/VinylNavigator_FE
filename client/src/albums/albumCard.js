8313cd7fc3bdd0074fc3b554f46082ea1aa2a27d
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-03-02 23:46:19 -0600

    hooked up axios on albumcards and data is being passed properly

--- client/src/albums/albumCard.js
+++ client/src/albums/albumCard.js
@@ -2,15 +2,15 @@ import React from 'react'
 import '../styles/albums/albumCard.css'
 
 export default class AlbumCard extends React.Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
     }
 
     render(){
         return(
             <div className='album-cards-container'>
                 <div className='image-container'>
-                    <img /> 
+                    <img src={this.props.albumCards.band_image}/> 
                 </div> 
                 <div className='content-container'>
                     <h6>This is the description section of the article being posted</h6>
--- client/src/albums/albumCards.js
+++ client/src/albums/albumCards.js
@@ -1,14 +1,43 @@
 import React from 'react'
+import axios from 'axios'
+import AlbumCard from './albumCard.js'
 import '../styles/albums/albumCards.css'
 
 export default class AlbumCards extends React.Component{
     constructor(){
         super()
+        this.state = {
+            albumCards: []
+        }
     }
 
+    componentDidMount(){
+        this.renderCards()
+    }
+
+    renderCards = () => {
+        axios.get(`${process.env.REACT_APP_POST_ALBUM}`)
+            .then(response => {
+                this.setState({
+                    albumCards: response.data
+                })
+            })
+            .catch(error => {
+                console.log("There was an error rendering your data", error)
+            })
+    }
+
+
     render(){
+        if(!this.state.albumCards){return 'loading data...'}
+        console.log(this.state.albumCards)
         return(
             <div>
+                {this.state.albumCards.map((item, index) => {
+                    return <AlbumCard albumCards={item}
+                               key={index}
+                               /> 
+                })}
                 
             </div>
         )
